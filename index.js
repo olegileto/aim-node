5d2b69a0259a0417f02a4722072f3f3860f17f53
Oleg Khmelevskoy <olegileto@gmail.com>
2019-03-20 16:58:59 +0200

    Connection to MySQL Server

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-   res.write('Hello');
-   res.end();
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "1111",
+    database: "node-schema"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected...');
+});
+
+app.get('/selectall', (req, res) => {
+    const selectAll  = 'SELECT * FROM workers';
+    connection.query(selectAll, (err, result) => {
+        if (err) { console.log(err); }
+        console.log(result);
+        res.send('Selected all...');
+    });
+
+    connection.end();
 });
 
 app.listen(port, (err) => err ? console.log(`Something is wrong ${err.message}`) : console.log(`Listening on port ${port}`));
